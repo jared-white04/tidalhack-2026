6c32e2c9b14cab4ed45018a3d2c627fb181bff2e
marcjuguilon <143764468+CaptainBullhark1@users.noreply.github.com>
2026-02-08 11:39:23 -0600

    Final Formatted changes

--- python-api/formatter.py
+++ python-api/formatter.py
@@ -289,22 +289,20 @@ ILI_2022_formatted.to_csv(file_path + '/ILI_2022_formatted.csv', index=False)
 with open(FILE, 'r') as file:
     data = json.load(file)
 
-# ili_objects = {
-#     '2007': ILI('Cleaned_ILIDataV2-2007.csv', data['ILI_2007']),
-#     '2015': ILI('Cleaned_ILIDataV2-2015.csv', data['ILI_2015']),
-#     '2022': ILI('Cleaned_ILIDataV2-2022.csv', data['ILI_2022'])
-# }
+ili_objects = {
+    '2007': ILI('Cleaned_ILIDataV2-2007.csv', data['ILI_2007'])
+}
 
-# for year, ili_obj in ili_objects.items():
-#     print(f"Processing {year}...")
+for year, ili_obj in ili_objects.items():
+    print(f"Processing {year}...")
     
-#     ili_obj.process()
+    ili_obj.process()
     
-#     ili_obj.filter_anomalies()
-#     ili_obj.generate_ids()
-#     ili_obj.clock_to_degrees()
-#     ili_obj.normalize_depth()
-#     ili_obj.clean_relative_position()
+    ili_obj.filter_anomalies()
+    ili_obj.generate_ids()
+    ili_obj.clock_to_degrees()
+    ili_obj.normalize_depth()
+    ili_obj.clean_relative_position()
     
-#     ili_obj.save_csv(f'ILI_{year}_formatted.csv')
-#     print(f"Finished {year}.")
+    ili_obj.save_csv(f'ILI_{year}_formatted.csv')
+    print(f"Finished {year}.")
